0bf5f04345ce4d4a8e64671dc58e60b266087fd1
woshiliuliang <31185143+woshiliuliang@users.noreply.github.com>
2023-05-17 14:25:18 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,38 +1,54 @@
-还在担心自己的代码风格不规范, python black让你省心省力
+除了print,你知道pprint吗？它能打印的更美观
 
-假设你写的代码是这样
-dic = {"a":1, "b":2,   "c":3, "d":"fffffffffffffffffffffffffffffffffffffffffffffffffffffff"}
+很多朋友知道print,但不知道pprint
+pprint和print都是python的打印模块，功能基本一样，唯一的区别就是
+pprint打印出来的数据结构更加美观漂亮，更加方便阅读打印输出结果。特别是对于特别长的数据打印
+print输出结果都在一行，不方便查看
 
-使用black之后,自动帮你改成：
+from pprint import pprint
+
 dic = {
-    "a": 1,
-    "b": 2,
-    "c": 3,
-    "d": "fffffffffffffffffffffffffffffffffffffffffffffffffffffff",
+    "王力宏": {
+        "部门": "科技部",
+        "工资": 3000,
+        "级别": 1
+    }, "周杰伦": {
+        "部门": "市场部",
+        "工资": 5000,
+        "级别": 2
+    }
 }
 
-Black，代码格式化工具，如果你的代码不符合PEP8规范，它检测到不符合规范的代码风格直接就帮你全部格式化好
-使用非常简单，安装成功后，和其他系统命令一样使用，只需在 black 命令后面指定需要格式化的文件或者目录就ok
+print(dic)
+'''
+输出
+{'王力宏': {'部门': '科技部', '工资': 3000, '级别': 1}, '周杰伦': {'部门': '市场部', '工资': 5000, '级别': 2}}
+'''
 
-安装
-pip install black
+pprint(dic)
+'''
+输出
+{'周杰伦': {'工资': 5000, '级别': 2, '部门': '市场部'},
+ '王力宏': {'工资': 3000, '级别': 1, '部门': '科技部'}}
+'''
 
-使用
-black my_file.py
+# width控制每行输出的宽度
 
-注意：
-black会格式化整个文件，不会格式化以#fmt:off 开头和以 #fmt:on 结尾的代码块，或者以 #fmt:skip 结尾的行
+pprint(dic,width=10)
+'''
+输出
+{'周杰伦': {'工资': 5000,
+         '级别': 2,
+         '部门': '市场部'},
+ '王力宏': {'工资': 3000,
+         '级别': 1,
+         '部门': '科技部'}}
+'''
 
-如果需要定制某些配置，可以编写pyproject.toml文件：
-[tool.black]
-line-length = 88
-include = '\.pyi?$'
+pprint(dic,indent=5)
+'''
+输出
+{    '周杰伦': {'工资': 5000, '级别': 2, '部门': '市场部'},
+     '王力宏': {'工资': 3000, '级别': 1, '部门': '科技部'}}
 
-还可以与 pre-commit 集成，集成后 git commit 时自动帮你检测代码并自动格式化，格式化后本次commit失败，需要自己重新 git add,git commit
-编辑.pre-commit-config.yaml文件
-repos:
-  - repo: local
-    hooks:
-      - id: black
-        name: black
-        language: system
+'''
