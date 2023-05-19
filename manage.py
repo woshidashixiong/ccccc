03312ecfc0f70510726c809d449fcff1f79ebb22
woshiliuliang <31185143+woshiliuliang@users.noreply.github.com>
2023-05-19 17:18:19 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,34 +1,34 @@
-import json
+#使用%或者format在变量多的时候一不小心就搞乱了位置，还得仔细的检查，用f-string就不用担心这些问题,而且很直观便于阅读理解
+# f-string，亦称为格式化字符串常量（formatted string literals），是Python3.6新引入的一种字符串格式化方法
+# 主要目的是使格式化字符串的操作更加简便。f-string在形式上是以 f 或 F 修饰符引领的字符串（f’xxx’ 或 F’xxx’），以大括号 {} 标明被替换的字段
 
-dic = {"status": "OK", "count": 20, "results": [{"age": 27, "name": "aex", "lactose_intolerant": True}, {"age": 29, "name": "jil", "lactose_intolerant": False}]}
-
-# 普通dump
-print(json.dumps(dic))
+name = "xiaotian"
+age = 10
+print(f"my name is {name},age {age}")
 '''
 输出
-{'status': 'OK', 'count': 20, 'results': [{'age': 27, 'name': 'aex', 'lactose_intolerant': True}, {'age': 29, 'name': 'jil', 'lactose_intolerant': False}]}
+my name is xiaotian,age 10
 '''
 
-# 加上 indention参数
-print(json.dumps(dic,indent=2))
+
+#  还可以填入表达式或调用函数，Python会求出其结果并填入返回的字符串内：
+print(f" 1 + 1 = {1+1}")
 '''
-{
-  "status": "OK",
-  "count": 20,
-  "results": [
-    {
-      "age": 27,
-      "name": "aex",
-      "lactose_intolerant": true
-    },
-    {
-      "age": 29,
-      "name": "jil",
-      "lactose_intolerant": false
-    }
-  ]
-}
+输出
+ 1 + 1 = 2
 '''
 
-这样无论是打印输出还是写入到文件都很美观，易于阅读
-
+# f-string还可用于多行字符串
+a,b,c = 1,2,3
+s = f'''
+ a is { a }
+ b is { b }
+ c is { c }
+'''
+print(s)
+'''
+输出
+ a is 1
+ b is 2
+ c is 3
+'''
