a6161a66a4f4fb169ad1ce58f9a3f0f779d545a5
woshiliuliang <31185143+woshiliuliang@users.noreply.github.com>
2023-05-22 17:20:16 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,18 +1,26 @@
-有的时候我们使用pip下载包的时候，会觉得下载速度十分慢，那是因为在默认情况下，pip使用的国外的源，想要下载速度快点的话，就需要把源换成国内的源。
+实战面试题：is和==的区别
 
-推荐国内源有以下几种：
-阿里云：http://mirrors.aliyun.com/pypi/simple/
-豆瓣：http://pypi.douban.com/simple/
-清华：https://pypi.tuna.tsinghua.edu.cn/simple/
-  
-  
-查看当前pip源 pip config list
-global.index-url='http://pypi.douban.com/simple/'
-global.timeout='6000'
-global.trusted-host='pypi.douban.com'
+is比较的是两个对象的地址值，也就是说两个对象是否为同一个实例对象；而==比较的是对象的值是否相等，其调用了对象的__eq__()方法。
 
-永久更改pip源
-pip config set global.index-url 源地址
+l1 = [1,2]
+l2 = [1,2]
 
-临时跟换
-pip install <包名> -i https://mirrors.tools.huawei.com/pypi/simple
+print(id(l1))
+'''
+2722890700032
+'''
+
+print(id(l2))
+'''
+2722890939776
+'''
+
+print(l1 == l2)
+'''
+True
+'''
+
+print(l1 is l2)
+'''
+False
+'''
