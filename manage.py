5514ecfd0832cbe593dc54597cf5fd40898253e0
woshiliuliang <31185143+woshiliuliang@users.noreply.github.com>
2023-05-23 13:58:01 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,2 +1,63 @@
-Flask和Django路由映射的区别？
-在django中，路由是浏览器访问服务器时，先访问的项目中的url，再由项目中的url找到应用中url，这些url是放在一个列表里，遵从从前往后匹配的规则。在flask中，路由是通过装饰器给每个视图函数提供的，而且根据请求方式的不同可以一个url用于不同的作用
+python性能分析工具cProfile,推荐给大家
+
+cProfile 是 python 代码调优的一种工具，它能够统计在整个代码执行过程中，每个函数调用的次数和消耗的时间。
+
+# test1.py
+import time
+
+def a():
+    time.sleep(1)
+
+
+def b():
+    time.sleep(2)
+
+
+def main():
+    a()
+    b()
+    time.sleep(5)
+
+if __name__ == "__main__":
+    # 脚本里使用
+    # import cProfile
+    # cProfile.run("main()")
+    main()
+    
+两种使用方式
+
+命令行使用
+python -m cProfile test1.py (-s ziduan)
+脚本里使用
+import cProfile
+cProfile.run("main()")
+
+输出解释
+ 9 function calls in 8.027 seconds
+
+   Ordered by: standard name
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        1    0.000    0.000    8.027    8.027 test1.py:1(<module>)
+        1    0.000    0.000    8.027    8.027 test1.py:11(main)
+        1    0.000    0.000    1.006    1.006 test1.py:3(a)
+        1    0.000    0.000    2.006    2.006 test1.py:7(b)
+        1    0.000    0.000    8.027    8.027 {built-in method builtins.exec}
+        3    8.027    2.676    8.027    2.676 {built-in method time.sleep}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+
+共有 9 次函数调用，耗时8.027秒。
+
+ncalls 函数的被调用次数 
+
+tottime 函数总计运行时间，除去函数中调用的函数运行时间 
+
+percall 函数运行一次的平均时间，等于tottime/ncalls 
+
+cumtime 函数总计运行时间，含调用的函数运行时间 
+
+percall 函数运行一次的平均时间，等于cumtime/ncalls 
+
+filename:lineno(function) 函数所在的文件名，函数的行号，函数名
+  
+  
