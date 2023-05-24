124cad24232c1a17df0513ac775cb2af639756e0
woshiliuliang <31185143+woshiliuliang@users.noreply.github.com>
2023-05-24 17:57:52 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,13 +1,8 @@
-实战面试题：字符串插值有多少种方法
+在vim中
+显示换行符等字符
+:set invlist
+关闭显示
+:set nolist
 
-有5种
 
-1 通过+号拼接
-
-2 通过(%)操作符拼接
-
-3 通过str.format()方法拼接
-
-4 通过F-strings拼接
-
-5 通过string模块中的Template对象拼接
+其中一个^I表示出来\t,$表示出来\n
