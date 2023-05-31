de6fd1118a2d81593968af500070c06495dbc979
woshiliuliang <31185143+woshiliuliang@users.noreply.github.com>
2023-05-31 17:51:28 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,19 +1,9 @@
-1.从本地将文件传输到服务器
-scp 【本地文件的路径】 【服务器用户名】@【服务器地址】:【要存放文件的路径】
+要求：写一个函数，传入的参数是一个列表（列表中的元素可能也是一个列表），返回该列表最大的嵌套深度。例如：列表[1, 2, 3]的嵌套深度为1，列表[[1], [2, [3]]]的嵌套深度为3。
 
-scp /usr/local/tools/test.png root@192.168.1.1:/usr/local/tools/
-
-2.从本地将文件夹传输到服务器
-scp -r 【本地文件的路径】 【服务器用户名】@【服务器地址】:【要存放文件夹的路径】
-
-scp -r /usr/local/tools/test root@192.168.1.1:/usr/local/tools
-
-3.将服务器上的文件传输到本地
-scp 【服务器用户名】@【服务器地址】:【服务器上存放文件的路径】 【本地文件的路径】
-
-scp root@192.168.1.1:/usr/local/tools/111.png /usr/local/tools
-
-4.将服务器上的文件夹传输到本地
-scp -r 【服务器用户名】@【服务器地址】:【服务器上存放文件的路径】【本地文件的路径】
-
-scp -r root@192.168.1.1:/data/wwwroot/default/test /
+def list_depth(items):
+    if isinstance(items, list):
+        max_depth = 1
+        for item in items:
+            max_depth = max(list_depth(item) + 1, max_depth)
+        return max_depth
+    return 0
