d8a8f2efcd5026fdb33b56ba79ae9390123ffafa
woshiliuliang <31185143+woshiliuliang@users.noreply.github.com>
2023-06-01 14:52:30 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,9 +1,20 @@
-要求：写一个函数，传入的参数是一个列表（列表中的元素可能也是一个列表），返回该列表最大的嵌套深度。例如：列表[1, 2, 3]的嵌套深度为1，列表[[1], [2, [3]]]的嵌套深度为3。
+python print出一个表格
 
-def list_depth(items):
-    if isinstance(items, list):
-        max_depth = 1
-        for item in items:
-            max_depth = max(list_depth(item) + 1, max_depth)
-        return max_depth
-    return 0
+有时候会有些需求在终端输出表格数据给用户展示，这时候就用到了PrettyTable
+非常好用，推荐给大家
+
+from prettytable import PrettyTable
+
+table = PrettyTable(["name","age","score"])
+table.add_row(["a","15","90"])
+table.add_row(["b","17","100"])
+print(table)
+'''
+输出
++------+-----+-------+
+| name | age | score |
++------+-----+-------+
+|  a   |  15 |   90  |
+|  b   |  17 |  100  |
++------+-----+-------+
+'''
