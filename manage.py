daeccef090899f0f0239716fbb852f3f354a4941
woshiliuliang <31185143+woshiliuliang@users.noreply.github.com>
2023-06-05 14:39:53 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,4 +1,21 @@
-for i in range(1, 10):
-    for j in range(1, i+1):
-        print('{}*{}={}	'.format(j, i, i*j), end='')
-    print()
+python 异常捕获里的“坑”
+
+def func():
+    try:
+        return 1
+    except:
+        return 2
+    else:
+        return 3
+    finally:
+        return 4
+
+你觉得调用func会返回哪一个值？
+实际上不是1，也不是3，即使try里面有异常也不是2
+
+print(func()) # 输出4
+
+原因是，在try…finally…语句中，因为要保证finally能够执行，所以try,except,else里的return无效
+
+在这里需要小心
+
