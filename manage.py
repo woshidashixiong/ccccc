46e83cf51b3d72ed7f81d87f13cb7f0aec05111a
woshiliuliang <31185143+woshiliuliang@users.noreply.github.com>
2023-06-09 13:51:23 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,45 +1,10 @@
-在 Flask 中设置接口超时时间可以通过使用装饰器来实现。具体步骤如下：
+from PIL import Image
+import io
 
-1. 导入 `signal` 模块：
+# 打开AI文件
+with open('example.ai', 'rb') as f:
+    ai_data = f.read()
 
-   ```python
-   import signal
-   from functools import wraps
-   ```
-
-2. 定义一个装饰器函数，用于设置超时时间：
-
-   ```python
-   def timeout(seconds=10, error_message='Timeout'):
-       def decorator(func):
-           def _handle_timeout(signum, frame):
-               raise TimeoutError(error_message)
-           @wraps(func)
-           def wrapper(*args, **kwargs):
-               signal.signal(signal.SIGALRM, _handle_timeout)
-               signal.alarm(seconds)
-               try:
-                   result = func(*args, **kwargs)
-               finally:
-                   signal.alarm(0)
-               return result
-
-           return wrapper
-
-       return decorator
-   ```
-
-   这个装饰器函数接受两个参数：`seconds` 表示超时时间，`error_message` 表示超时时抛出的异常信息。
-
-3. 在需要设置超时时间的接口函数上使用装饰器：
-
-   ```python
-   @app.route('/api/some_endpoint')
-   @timeout(seconds=5)
-   def some_endpoint():
-       # 接口函数的实现
-   ```
-
-   在这个例子中，`some_endpoint` 函数的超时时间被设置为 5 秒。
-
-注意：在使用 `signal` 模块时，需要注意操作系统的限制。在windows操作系统中，`signal` 模块可能无法正常工作。
+# 将AI文件转换为PNG格式
+with Image.open(io.BytesIO(ai_data)) as im:
+    im.save('example.png', 'PNG')
