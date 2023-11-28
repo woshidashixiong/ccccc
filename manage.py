9d6d93cc2d1bb3d820ed22f1e06ea21ec741b7bb
woshidashixiong <31185143+woshidashixiong@users.noreply.github.com>
2023-11-28 11:26:26 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,22 +1,43 @@
-题目004：假设你使用的是官方的CPython，说出下面代码的运行结果。
+Python Schedule库是一种用于在特定时间或时间间隔内执行任务的工具。它可以让开发者轻松地编写和管理重复性任务，比如定时发送邮件、定时备份数据、定时清理文件等。
 
-a, b, c, d =1, 1, 1000, 1000
-print(a is b, c is d)
+Python Schedule库的用法非常简单。首先，需要安装Schedule库，可以使用pip install schedule命令进行安装。安装完成后，就可以开始编写代码了。
 
-def func():
-    e = 1000
-    f = 1000
-    print(e is f, e is d)
-    g = 1
-    print(g is a)
+下面是一个简单的示例代码，用于在每天的固定时间执行任务：
 
-func()
+```python
+import schedule
+import time
 
-True False
-True False
-True
+def job():
+    print("I'm working...")
 
-上面代码中a is b的结果是 True 但c is d的结果是 False,这一点的确让人费解。CPython解释器出于性能优化的考虑,把频繁使用的
-整数对象用一个叫 small_ints 的对象池缓存起来造成的。 small_ints 缓存的整数值被设定为[-5,256】这个区间,也就是说,在任何引
-用这些整数的地方,都不需要重新创建int 对象,而是直接引用缓存池中的对象。如果整数不在该范围内,那么即便两个整数的值相同,
-它们也是不同的对象。
+schedule.every().day.at("10:30").do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+```
+
+在这个示例代码中，我们定义了一个名为job的函数，它的作用是打印一条信息。然后，我们使用schedule.every().day.at("10:30").do(job)来指定任务的执行时间，也就是每天的10:30执行一次job函数。最后，我们使用一个无限循环来不断检查任务是否需要执行，如果需要就执行它。
+
+除了每天的固定时间，Python Schedule库还支持其他的时间间隔，比如每隔一段时间执行一次任务。下面是一个示例代码，用于每隔5秒执行一次任务：
+
+```python
+import schedule
+import time
+
+def job():
+    print("I'm working...")
+
+schedule.every(5).seconds.do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+```
+
+在这个示例代码中，我们使用schedule.every(5).seconds.do(job)来指定任务的执行时间，也就是每隔5秒执行一次job函数。
+
+Python Schedule库还支持其他的时间间隔，比如每小时、每周、每月等等。开发者可以根据自己的需求来选择合适的时间间隔。
+
+总之，Python Schedule库是一种非常实用的工具，可以让开发者轻松地管理重复性任务，提高工作效率。如果你需要编写定时任务的程序，不妨试试Python Schedule库吧！
