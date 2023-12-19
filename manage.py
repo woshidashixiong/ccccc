1f2054a9c956ce30a50bd27b1fa06ac90cdf2303
woshidashixiong <31185143+woshidashixiong@users.noreply.github.com>
2023-12-19 09:49:15 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,43 +1,19 @@
-Python Schedule库是一种用于在特定时间或时间间隔内执行任务的工具。它可以让开发者轻松地编写和管理重复性任务，比如定时发送邮件、定时备份数据、定时清理文件等。
 
-Python Schedule库的用法非常简单。首先，需要安装Schedule库，可以使用pip install schedule命令进行安装。安装完成后，就可以开始编写代码了。
+git pull origin master 报错：fatal: Unable to find remote helper for 'http'
 
-下面是一个简单的示例代码，用于在每天的固定时间执行任务：
 
-```python
-import schedule
-import time
+尝试以下解决方案：
 
-def job():
-    print("I'm working...")
+1. 升级Git版本到最新版本。
 
-schedule.every().day.at("10:30").do(job)
+2. 确保您的Git配置正确。您可以使用以下命令检查您的Git配置：
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
-```
+   ```
+   git config --list
+   ```
 
-在这个示例代码中，我们定义了一个名为job的函数，它的作用是打印一条信息。然后，我们使用schedule.every().day.at("10:30").do(job)来指定任务的执行时间，也就是每天的10:30执行一次job函数。最后，我们使用一个无限循环来不断检查任务是否需要执行，如果需要就执行它。
+   确保您的远程URL是正确的，并且格式正确。
 
-除了每天的固定时间，Python Schedule库还支持其他的时间间隔，比如每隔一段时间执行一次任务。下面是一个示例代码，用于每隔5秒执行一次任务：
+3. 如果您使用的是Windows操作系统，请确保您的Git安装路径已添加到系统环境变量中。
 
-```python
-import schedule
-import time
-
-def job():
-    print("I'm working...")
-
-schedule.every(5).seconds.do(job)
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
-```
-
-在这个示例代码中，我们使用schedule.every(5).seconds.do(job)来指定任务的执行时间，也就是每隔5秒执行一次job函数。
-
-Python Schedule库还支持其他的时间间隔，比如每小时、每周、每月等等。开发者可以根据自己的需求来选择合适的时间间隔。
-
-总之，Python Schedule库是一种非常实用的工具，可以让开发者轻松地管理重复性任务，提高工作效率。如果你需要编写定时任务的程序，不妨试试Python Schedule库吧！
+4. 如果您使用的是代理，请确保您已正确配置代理。
