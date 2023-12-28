8e4944c39e9272e6c6c0f942ed7d9003446655b2
woshidashixiong <31185143+woshidashixiong@users.noreply.github.com>
2023-12-28 17:03:51 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,19 +1,11 @@
+写一个删除列表中重复元素的函数，要求去重后元素相对位置保持不变。
 
-git pull origin master 报错：fatal: Unable to find remote helper for 'http'
 
-
-尝试以下解决方案：
-
-1. 升级Git版本到最新版本。
-
-2. 确保您的Git配置正确。您可以使用以下命令检查您的Git配置：
-
-   ```
-   git config --list
-   ```
-
-   确保您的远程URL是正确的，并且格式正确。
-
-3. 如果您使用的是Windows操作系统，请确保您的Git安装路径已添加到系统环境变量中。
-
-4. 如果您使用的是代理，请确保您已正确配置代理。
+def dedup(items):
+    no_dup_items = []
+    seen = set()
+    for item in items:
+        if item not in seen:
+            no_dup_items.append(item)
+            seen.add(item)
+    return no_dup_items
