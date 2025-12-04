3bb6f5ad5fe3a694fd5a0aa485124284fa52d534
woshidashixiong <31185143+woshidashixiong@users.noreply.github.com>
2025-12-04 15:03:15 +0800

    Update a.py

--- a.py
+++ a.py
@@ -1,1 +1,10 @@
-修改了a.py
+import hmac
+import base64
+import hashlib
+key="123"
+secret="wwwww"
+string_to_sign="key=123/users/get"
+def generate_sign(string_to_sign, secret):
+    hmac_sha256 = hmac.new(secret.encode('utf-8'), string_to_sign.encode('utf-8'), hashlib.sha256)
+    hmac_bytes = hmac_sha256.digest()
+    return base64.b64encode(hmac_bytes).decode('utf-8')
